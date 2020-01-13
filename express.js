6edcdb944fffc8404af4975436b7f86b8ce9c2a0
Adam Gardner <adam.gardner@dynatrace.com>
2020-01-13 13:51:46 +1100

    Create express.js

---
+++ express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const filesystem = require('fs');
+const app = express();
+
+const port = 80;
+
+function renderPage(res) {
+  res.sendFile(__dirname + '/index.html'); 
+}
+app.listen(port, function() {
+  console.log('listening on port ' + port);
+});
+ 
+app.get('/', (req, res) => {
+    setTimeout(renderPage, 0, res);
+});
